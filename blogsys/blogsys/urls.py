3956657f02c3b99550dec87c1cff08e813a299c1
blodman <475131479@qq.com>
2018-05-29 13:33:53 +0800

    增加rest api支持(文章、分类、标签、用户关联数据)

--- blogsys/blogsys/urls.py
+++ blogsys/blogsys/urls.py
@@ -1,5 +1,6 @@
 import xadmin
 from xadmin.plugins import xversion
+from rest_framework import routers, documentation
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,11 +9,18 @@ from django.conf.urls import url, include
 from blog.views import IndexView, CategoryView, TagView, PostView, AuthorView
 from config.views import LinkView
 from comment.views import CommentView
+from blog.api import PostViewSet, CategoryViewSet, TagViewSet, UserViewSet
 # from config.views import links
 
 
 xversion.register_models()
 xadmin.autodiscover()
+
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'cats', CategoryViewSet)
+router.register(r'tags', TagViewSet)
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name="index"),
@@ -25,4 +33,6 @@ urlpatterns = [
     # url(r'^cus_admin/', custom_site.urls),
     url(r'^comment/$', CommentView.as_view(), name="Comment"),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^api/', include(router.urls)),
+    url(r'^api/docs/', documentation.include_docs_urls(title='blogsys apis')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

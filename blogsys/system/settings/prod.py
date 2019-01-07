355dc983d18531292cb5c87cfa0a670b437cbe0a
zhangqian <zhangqian@inad.com>
2019-01-07 17:08:12 +0800

    Begin upload migrtions to git

--- blogsys/system/settings/prod.py
+++ blogsys/system/settings/prod.py
@@ -7,7 +7,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog',
-        'USER': 'root',
+        'USER': 'blog',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': 3306,

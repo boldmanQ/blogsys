b38d0e6a62bed4bc24ef4cbc390c42d7a5d3b06f
zhangqian <zhangqian@inad.com>
2019-01-07 10:29:37 +0800

    Fix Posts admin page some search_fields display other user's data

--- blogsys/system/admin.py
+++ blogsys/system/admin.py
@@ -3,6 +3,8 @@
 # author:zq time:2018/3/15
 from django.contrib import admin
 
+from blog.models import Category
+
 
 class BaseOwnerAdmin(admin.ModelAdmin):
     '''
@@ -14,6 +16,11 @@ class BaseOwnerAdmin(admin.ModelAdmin):
         if request.user.is_superuser:
             return queryset.all()
         return queryset.filter(owner=request.user)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category' and not request.user.is_superuser:
+            kwargs["queryset"] = Category.objects.filter(owner=request.user)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
--- blogsys/system/settings/prod.py
+++ blogsys/system/settings/prod.py
@@ -38,3 +38,26 @@ DEBUG = False
 #INTERNAL_IPS = ['117.136.88.36']
 #DEBUG_TOOLBAR_PANELSOOLBAR_PANELS = (
 #)
+
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static_files'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, THEME_TEMPLATE_DIR, 'static')
+]
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'full',
+        'extraPlugins': 'codesnippet,uploadimage,prism,widget,lineutils',
+        'height': 300,
+        'width': 1000,
+        'tabSpaces': 4,
+    },
+}
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "/data/www/blogsys/media"
+CKEDITOR_UPLOAD_PATH = "images"
+DEFAULT_FILE_STORAGE = 'blogsys.storage.MyStorage'
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 24
+}
